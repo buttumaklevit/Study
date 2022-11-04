0c5c94b321a226469f0158347f45a21604c68df9
buttumaklevit <106801888+buttumaklevit@users.noreply.github.com>
2022-11-04 13:27:47 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,6 +1,11 @@
 def solution(my_str, n):
     answer = []
-    for _ in range(0, len(my_str), n):
-        answer.append(my_str[:n])
-        my_str = my_str[n:]
+
+    while my_str:
+        if len(my_str)>=n:
+            answer.append(my_str[:n])
+            my_str=my_str[n:]
+        elif len(my_str)<n:
+            answer.append(my_str)
+            my_str=[]
     return answer
